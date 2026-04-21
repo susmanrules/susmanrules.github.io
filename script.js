e448965fb1c6a7be0b8868c491fb234907cb8b30
susmanrules <harijsmadzulis@gmail.com>
2026-04-21 16:17:33 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-fetch('particlesjs.json')
-  .then(response => response.json())
-  .then(config => {
-    particlesJS('particles-js', config);
-  })
-  .catch(error => console.error('Error loading particles config:', error));
